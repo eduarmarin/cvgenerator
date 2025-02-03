4049278de3475017419084c99703adaa059e97b8
unknown <donsecun@gmail.com>
2025-02-03 20:53:08 +0100

    traying to create second experience div

--- src/files/experience.jsx
+++ src/files/experience.jsx
@@ -7,61 +7,41 @@ const info = [
   { label: 'Phone', type: 'number', name: 'phone'},
   { label: 'Email', type: 'text', name: 'email'}
 ];
-
-export function Info() {
+export function Info() { // function to display the info form in column on the left
   return (
     <div className="info common">
-      {info.map((product) => (
-        <label key={product.name}>{product.label}:
-          <input type={product.type} name={product.name} />
+      {info.map((product0) => (
+        <label key={product0.name}>{product0.label}
+          <input type={product0.type} name={product0.name} />
         </label>
       ))}
     </div>
   )
 }
+
 const infoexper = [
   { label: 'Company', type: 'text', name: 'company'},                                                       
   { label: 'Functions', type: 'textarea', name: 'functions'},
   { label: 'Date', type: 'number', name: 'date'}
 ];
-export	function Experience() {
-  const [textarea, setTextarea] = useState("The content of a textarea goes in the value attribute")
-  const handleClick = () => {console.log('this is:', this)}
+export	function Experience() { // function to display the experience form in column on the left down info form
+  const [textarea] = useState("The content of a textarea goes in the value attribute")
+  const handleClick = () => {console.log('this is:')}
   return (
     <div className="experience common">
-      {infoexper.map((product) => (
-        <label key={product.name}>{product.label}:
-          {product.type == 'textarea'  ? (
-            <textarea type={product.type} name={product.name} />
+      {infoexper.map((product1) => (
+        <label key={product1}>{product1.label}
+          {product1.type == 'textarea'  ? (
+            <textarea type={product1.type} name={product1.name} />
             ) : (
-              <input type={product.type} name={product.name} />
+              <input type={product1.type} name={product1.name} />
             )}
         </label>
       ))}
       <button onClick={handleClick}>Add Experience</button>
     </div>
+    
   )
 }
-/* export function Experience() {
-  const [textarea, setTextarea] = useState("The content of a textarea goes in the value attribute")
-  const handleClick = () => {
-    console.log('this is:', this)
-  }
-  return (
-      <div className="experience common">
-        <label>Company:
-          <input type="text" name="name" />
-        </label>
-        <label>Functions:
-          <textarea value= {textarea} />
-        </label>
-        <label>Date:
-          <input type="number" name="phone" />
-        </label>
-        <button onClick={handleClick}>Add Experience</button>
-      </div>
-  )
-} */
-
 
 //export {Info, Experience}
